d6ff405746bf4489a703f7656aae81469656805f
杨金明 <30614179+yangjinming1062@users.noreply.github.com>
2020-04-03 11:11:22 +0800

    Update

---
+++ MulTools/ExtendControls.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace MulTools
+{
+    public class ActivableButton : Button
+    {
+        public static readonly DependencyProperty IsActivedProperty = DependencyProperty.Register("IsActived", typeof(bool), typeof(ActivableButton), new PropertyMetadata(default(bool)));
+        public bool IsActived
+        {
+            get => (bool)GetValue(IsActivedProperty);
+            set => SetValue(IsActivedProperty, value);
+        }
+    }
+
+    public class ColorPicker : ActivableButton
+    {
+        public static readonly DependencyProperty SizeProperty = DependencyProperty.Register("Size", typeof(ColorPickerButtonSize), typeof(ColorPicker),
+            new PropertyMetadata(default(ColorPickerButtonSize), OnColorPickerSizeChanged));
+
+        public ColorPickerButtonSize Size
+        {
+            get => (ColorPickerButtonSize)GetValue(SizeProperty);
+            set => SetValue(SizeProperty, value);
+        }
+
+        private static void OnColorPickerSizeChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
+        {
+            var v = (ColorPickerButtonSize)eventArgs.NewValue;
+            ColorPicker obj = dependencyObject as ColorPicker;
+            if (obj == null) return;
+            var w = 0.0;
+            switch (v)
+            {
+                case ColorPickerButtonSize.Small:
+                    w = (double)Application.Current.Resources["ColorPickerSmall"];
+                    break;
+                case ColorPickerButtonSize.Middle:
+                    w = (double)Application.Current.Resources["ColorPickerMiddle"];
+                    break;
+                default:
+                    w = (double)Application.Current.Resources["ColorPickerLarge"];
+                    break;
+            }
+            obj.BeginAnimation(WidthProperty, new DoubleAnimation(w, (Duration)Application.Current.Resources["Duration3"]));
+        }
+    }
+
+    public class CornerRadiusAnimation : AnimationTimeline
+    {
+        static CornerRadiusAnimation()
+        {
+            FromProperty = DependencyProperty.Register("From", typeof(CornerRadius), typeof(CornerRadius));
+            ToProperty = DependencyProperty.Register("To", typeof(CornerRadius), typeof(CornerRadius));
+        }
+
+        private bool _fromSetted;
+        private bool _toSetted;
+        public static readonly DependencyProperty FromProperty;
+        public CornerRadius From
+        {
+            get => (CornerRadius)GetValue(FromProperty);
+            set
+            {
+                SetValue(FromProperty, value);
+                _fromSetted = true;
+            }
+        }
+        public static readonly DependencyProperty ToProperty;
+        public CornerRadius To
+        {
+            get => (CornerRadius)GetValue(ToProperty);
+            set
+            {
+                SetValue(ToProperty, value);
+                _toSetted = true;
+            }
+        }
+        public override object GetCurrentValue(object defaultOriginValue, object defaultDestinationValue, AnimationClock animationClock)
+        {
+            var fromVal = _fromSetted ? (CornerRadius)GetValue(FromProperty) : (CornerRadius)defaultOriginValue;
+            var toVal = _toSetted ? (CornerRadius)GetValue(ToProperty) : (CornerRadius)defaultDestinationValue;
+            if (animationClock.CurrentProgress != null)
+                return new CornerRadius(
+                    animationClock.CurrentProgress.Value * (toVal.TopLeft - fromVal.TopLeft) + fromVal.TopLeft,
+                    animationClock.CurrentProgress.Value * (toVal.TopRight - fromVal.TopRight) + fromVal.TopRight,
+                    animationClock.CurrentProgress.Value * (toVal.BottomRight - fromVal.BottomRight) + fromVal.BottomRight,
+                    animationClock.CurrentProgress.Value * (toVal.BottomLeft - fromVal.BottomLeft) + fromVal.BottomLeft);
+            return new CornerRadius();
+        }
+        protected override Freezable CreateInstanceCore() => new CornerRadiusAnimation();
+
+        public override Type TargetPropertyType => typeof(CornerRadius);
+    }
+}
---
+++ MulTools/Models.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Ink;
+
+namespace MulTools
+{
+    public enum DrawMode
+    {
+        None = 0,
+        Select,
+        Pen,
+        Text,
+        Line,
+        Arrow,
+        Rectangle,
+        Circle,
+        Ray,
+        Erase
+    }
+    public enum StrokesHistoryNodeType
+    {
+        Removed,
+        Added
+    }
+    public enum ColorPickerButtonSize
+    {
+        Small,
+        Middle,
+        Large
+    }
+    internal class StrokesHistoryNode
+    {
+        public StrokeCollection Strokes { get; private set; }
+        public StrokesHistoryNodeType Type { get; private set; }
+
+        public StrokesHistoryNode(StrokeCollection strokes, StrokesHistoryNodeType type)
+        {
+            Strokes = strokes;
+            Type = type;
+        }
+    }
+
+    public class FileShowInfo
+    {
+        public string FilePath { get; set; }
+        public string FileName { get; set; }
+        public string FileType { get; set; }
+        public string FileSize { get; set; }
+        public string FileAddInfo { get; set; }
+    }
+}
