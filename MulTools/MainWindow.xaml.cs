48a5fa08adb9ebacc4af404f1bfc60c1bd47b433
杨金明 <30614179+yangjinming1062@users.noreply.github.com>
2020-04-08 18:36:22 +0800

    第四集

--- MulTools/MainWindow.xaml.cs
+++ MulTools/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Interop;
 
 namespace MulTools
 {
@@ -20,9 +21,21 @@ namespace MulTools
     /// </summary>
     public partial class MainWindow : Window
     {
+        System.Windows.Forms.Screen Screen;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Screen = System.Windows.Forms.Screen.FromHandle(new WindowInteropHelper(this).Handle);
+            LockPlace();
+        }
+        private void LockPlace()
+        {
+            this.Left = Screen.WorkingArea.X + (Screen.WorkingArea.Width - 190);
+            this.Top = Screen.WorkingArea.Y + (Screen.WorkingArea.Height - ActualHeight);
         }
     }
 }
