c4b21658d2084e29ccf2634e4d2e8047c94a920c
杨金明 <30614179+yangjinming1062@users.noreply.github.com>
2020-04-25 13:32:21 +0800

    第10集

---
+++ MulTools/Win/定时关机.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MulTools.Win
+{
+    /// <summary>
+    /// 定时关机.xaml 的交互逻辑
+    /// </summary>
+    public partial class 定时关机 : Window
+    {
+        public 定时关机()
+        {
+            InitializeComponent();
+        }
+    }
+}
