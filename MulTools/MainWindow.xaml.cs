6daa9c49e099f6e8bcf5d14a5ff4afc05c5bc88c
杨金明 <30614179+yangjinming1062@users.noreply.github.com>
2020-05-01 16:59:53 +0800

    第11.12.13集

--- MulTools/MainWindow.xaml.cs
+++ MulTools/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Interop;
 using System.Windows.Media.Animation;
 
@@ -32,6 +22,7 @@ namespace MulTools
         {
             Screen = System.Windows.Forms.Screen.FromHandle(new WindowInteropHelper(this).Handle);
             LockPlace();
+            BtOpen_Click(null, null);
         }
         private void LockPlace()
         {
--- MulTools/Win/文件操作.xaml.cs
+++ MulTools/Win/文件操作.xaml.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace MulTools.Win
 {
@@ -21,5 +12,13 @@ namespace MulTools.Win
         {
             InitializeComponent();
         }
+
+        private void TitleGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void btClose_Click(object sender, RoutedEventArgs e) => Close();
     }
 }
